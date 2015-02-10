8418370d5db240286683b79d40839d3ef72a9784
root <root@appstack-dev.(none)>
2015-02-10 20:48:29 +0800

    save order

---
+++ db/migrate/20150210063528_create_order_items.rb
@@ -0,0 +1,11 @@
+class CreateOrderItems < ActiveRecord::Migration
+  def change
+    create_table :order_items do |t|
+      t.string :itemUUID
+      t.string :product_name
+      t.string :packing
+
+      t.timestamps
+    end
+  end
+end
