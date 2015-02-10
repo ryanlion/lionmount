a810308fbdfcfd94cc966f3b3434bc4eea4b4486
Ryan <ryansc@Ryans-MacBook-Pro.local>
2015-02-10 23:37:41 +0800

    upload url

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
     protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
     def create
+        http://www.tutorialspoint.com/ruby-on-rails/rails-file-uploading.htm
         orders = JSON.parse(request.raw_post)
         orders["order_items"].each do |order|
           @order_item = OrderItem.new(order)
