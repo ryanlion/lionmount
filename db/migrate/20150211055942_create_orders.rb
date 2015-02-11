6794ac96833c268a7345759f913d6a1fd534c513
root <root@appstack-dev.(none)>
2015-02-11 17:48:42 +0800

    20150211

---
+++ db/migrate/20150211055942_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :customer_name
+      t.string :supplier_name
+
+      t.timestamps
+    end
+  end
+end
