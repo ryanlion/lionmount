7ccdf7f5e29c4ea2dd76b2c8e8c30ede1b330a3f
System Administrator <root@Ryans-MacBook-Pro.local>
2015-03-05 23:44:34 +0800

    20150305

--- db/migrate/20150304102418_create_shipments.rb
+++ db/migrate/20150304102418_create_shipments.rb
@@ -2,7 +2,8 @@ class CreateShipments < ActiveRecord::Migration
   def change
     create_table :shipments do |t|
       t.string :order_id
-
+      t.string :shipment_uuid
+      
       t.timestamps
     end
   end
