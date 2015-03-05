d70145399ad4131662db889d125db9e4a39d2c55
ryanlion <ryansc1983@gmail.com>
2015-03-05 20:03:01 +0800

    20150305

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,2 +1,8 @@
 class Shipment < ActiveRecord::Base
+  has_many :order, though: :relation_shipment_order
+  accepts_nested_attributes_for :order,
+    :allow_destroy => true
+  accepts_nested_attributes_for :ingredients,
+    :allow_destroy => true
+
 end
