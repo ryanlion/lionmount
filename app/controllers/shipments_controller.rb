36bf6e6d0997f20130ad6438276c7b1f9380ee1d
ryanlion <ryansc1983@gmail.com>
2015-03-06 22:13:16 +0800

    20150306

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -2,6 +2,14 @@ class ShipmentsController < ApplicationController
     protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
     def create
         debugger
+        orders = params["selected"]
+        uuid = params["ship-uuid"]
+        orders.each do |order|
+          @shipment = Shipment.new
+          @shipment.order_id = order.order_id
+
+          order
+        end
         render :text => "success"
     end
 end
