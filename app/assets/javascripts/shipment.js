19a812ad37c9b4f068dae8ee0e5e96ab20fb8346
ryanlion <ryansc1983@gmail.com>
2015-03-06 21:17:52 +0800

    20150306

--- app/assets/javascripts/shipment.js
+++ app/assets/javascripts/shipment.js
@@ -1,7 +1,23 @@
-$(document).on('submit','#shipment-form', function(){
-  $('.row-selector:checkbox:checked').each(function () {
-                                           alert($(this).val());
-    $(this).closest(".order-id").remove();
+$(document).on('click','.row-selector-shipment', function(){
+  $this = $(this);
+  $orderId = $this.next('.order-id')
+  $shipmentUuid = $orderId.next('.shipment-uuid')
+  if($this.is(":checked")){
+    $this.next('.order-id').attr('name','selected_id[]');
+    $orderId.attr('name','selected[]order_id');
+    $shipmentUuid.attr('name','selected[]shipment_uuid');
+  }else{
+    $orderId.attr('name','not_selected[]order_id');
+    $shipmentUuid.attr('name','not_selected[]shipment_uuid');
+  }
+});
+$(document).on('submit','#shipment-form',function(){
+  var new_uuid = uuid.v1({
+    node: [0x01, 0x23, 0x45, 0x67, 0x89, 0xab],
+      clockseq: 0x1234,
+      msecs: new Date().getTime(),
+      nsecs: 5678
   });
-               $("#shipment-form").submit();
+  alert($(".ship-uuid").val());
+  $(".ship-uuid").val(new_uuid);
 });
--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,7 +1,7 @@
 class ShipmentsController < ApplicationController
     protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
     def create
-        byebug
+        debugger
         render :text => "success"
     end
 end
