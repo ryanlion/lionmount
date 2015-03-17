31b9a0fdd659301257a2c5e380f364ebac50fe49
ryanlion <ryansc1983@gmail.com>
2015-03-17 12:33:28 +0800

    20150317

--- db/migrate/20150210063528_create_order_items.rb
+++ db/migrate/20150210063528_create_order_items.rb
@@ -8,6 +8,17 @@ class CreateOrderItems < ActiveRecord::Migration
       t.string :packing
       t.string :image_uid
       t.string :title
+      t.string :weight_per_product
+      t.string :color
+      t.string :quantiry_per_unit
+      t.string :unit
+      t.string :no_of_unit
+      t.string :volume_per_unit
+      t.string :item_total_weight
+      t.string :weight_per_unit
+      t.string :item_total_price
+      t.string :item_total_volume
+      t.string :remarks
 
       t.timestamps
     end
